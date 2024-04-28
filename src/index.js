7e8f116703ba74b3d2cb48ed154453dec0c8ca38
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-04-27 20:03:05 -0400

    Passing and receiving props

--- src/index.js
+++ src/index.js
@@ -71,8 +71,29 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+      <Pizza name='Pizza Funghi' ingredients='Tomato,mushrooms ' photoName='pizzas/funghi.jpg' price={12} />
     </main>
+  );
+}
+
+// Creating and Reusing a Component
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -81,20 +102,12 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   // if (hour >= openHour && hour <= closeHour) alert(`We're currently open!`);
   // else alert(`We're close!`);
-  return <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>;
-}
 
-// Creating and Reusing a Component
-function Pizza() {
-  return (
-    <footer className='footer'>
-      <img src='pizzas/spinaci.jpg' alt='Pizza spinaci' />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </footer>
-  );
+  return <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>;
 }
 
 // React v18
