a4057b3f4a30770af338d99810a47fe311240327
Gabriel <gabriel.duarte18@outlook.es>
2024-05-12 10:50:31 -0400

    Setting Classes and Text Conditionally

--- src/index.js
+++ src/index.js
@@ -76,11 +76,17 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 && (
-        <ul className='pizzas'>
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone even, all organic, all
+            delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       )}
       {/* <Pizza
         name='Pizza Spinaci'
@@ -94,16 +100,16 @@ function Menu() {
 }
 
 // Creating and Reusing a Component
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   // console.log(props);
-  if (props.pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
   return (
-    <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
